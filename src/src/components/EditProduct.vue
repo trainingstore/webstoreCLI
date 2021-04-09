941a9c7fa9b47d1c1b0a09acbd7bae02a454981c
Serge <foravitosells@gmail.com>
2021-04-09 22:27:59 +0300

    initial commit

--- src/src/components/EditProduct.vue
+++
@@ -1,9 +0,0 @@
-<template src="./EditProduct.html"></template>
-<style src="./EditProduct.css"></style>
-<script>
-  export default {
-    created: function () {
-      console.log('edit mode');
-    }
-  }
-</script>
--- src/src/db.js
+++
@@ -1,6 +0,0 @@
-// import firebase from 'firebase/app'
-// import 'firebase/database'
-//
-// export const db = firebase
-//   .initializeApp({ databaseURL: 'https://webstore-c9973-default-rtdb.firebaseio.com/'})
-//   .database()
